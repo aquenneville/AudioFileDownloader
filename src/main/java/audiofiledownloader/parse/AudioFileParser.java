286a8d79fe12af8115472af5cb0072389d993700
aquenneville <alainquenneville@gmail.com>
2018-03-05 22:33:37 +0000

    adding the main/java sub directories

---
+++ src/main/java/audiofiledownloader/parse/AudioFileParser.java
@@ -0,0 +1,39 @@
+package audiofiledownloader.parse;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class AudioFileParser {
+
+	public static List<String> parse(Path pagePath) {
+		List<String> resourceList = new ArrayList<>();
+		//String[] resourceList = null;
+		byte[] byteContent;
+		try {
+			byteContent = Files.readAllBytes(pagePath);
+			String stringContent = new String(byteContent);
+			// <a([^>]+)>
+			//resourceList = stringContent.split("<a[^>]*>");
+			Document doc = Jsoup.parse(stringContent);
+			Elements elmts = doc.select("a");
+			for (Element elmt: elmts) {	
+				if (elmt.attr("href").matches(".*mp3|.*mp4")) {
+					resourceList.add(elmt.attr("href"));
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return resourceList;		
+	}
+
+}
